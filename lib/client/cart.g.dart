a3694c51f2f663c83551194c665057e0329e276b
thaiha1607 <halequocthai@outlook.com>
2024-04-08 18:47:30 +0700

    fix: Remove generated files from gitignore

---
+++ lib/client/cart.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cart.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$cartNotifierHash() => r'b9ebdb392d19e634171d9672fae3ed163eaa701a';
+
+/// See also [CartNotifier].
+@ProviderFor(CartNotifier)
+final cartNotifierProvider =
+    AutoDisposeNotifierProvider<CartNotifier, List<OrderItem>>.internal(
+  CartNotifier.new,
+  name: r'cartNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$cartNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CartNotifier = AutoDisposeNotifier<List<OrderItem>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/client/payment.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'payment.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$orderedProductNotifierHash() =>
+    r'0505d885e55577ea3c65663c8ba04294435ca6d8';
+
+/// See also [OrderedProductNotifier].
+@ProviderFor(OrderedProductNotifier)
+final orderedProductNotifierProvider =
+    AutoDisposeNotifierProvider<OrderedProductNotifier, List<Product>>.internal(
+  OrderedProductNotifier.new,
+  name: r'orderedProductNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$orderedProductNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$OrderedProductNotifier = AutoDisposeNotifier<List<Product>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
