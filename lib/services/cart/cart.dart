ac2c8d5e0be57e6de4b88713a2fd2e0c0e8b522f
thaiha1607 <halequocthai@outlook.com>
2024-07-11 16:36:07 +0700

    fix: Annotate Cart as `unfreezed`. Change method signatures.
    - Annotate Cart as `unfreezed`.
    - Remove CartCreation
    - Change method signatures for {add,delete,deleteAll}OrderProduct

--- lib/services/cart/cart.dart
+++ lib/services/cart/cart.dart
@@ -5,7 +5,7 @@ import '../order/models/order_product.dart';
 part 'cart.freezed.dart';
 part 'cart.g.dart';
 
-@freezed
+@unfreezed
 class Cart with _$Cart {
   const Cart._();
 
@@ -19,24 +19,17 @@ class Cart with _$Cart {
 
   factory Cart.fromJson(Map<String, Object?> json) => _$CartFromJson(json);
 
-  Cart addOrderProduct(OrderProduct orderItem) {
-    return copyWith(
-      listOrderProduct: [...listOrderProduct, orderItem],
-    );
+  void addOrderProduct(OrderProduct orderItem) {
+    listOrderProduct.add(orderItem);
   }
 
-  Cart deleteOrderProduct(OrderProduct orderItem) {
-    return copyWith(
-      listOrderProduct: List<OrderProduct>.from(listOrderProduct)
-        ..remove(orderItem),
-    );
+  void deleteOrderProduct(OrderProduct orderItem) {
+    listOrderProduct.remove(orderItem);
   }
 
-  Cart deleteAllOrderProduct() {
-    return copyWith(
-      listOrderProduct: [],
-      totalCost: 0,
-    );
+  void deleteAllOrderProduct() {
+    listOrderProduct.clear();
+    totalCost = 0;
   }
 
   static int? calculateTotalCost(List<OrderProduct> itemsInCart) {
@@ -54,16 +47,3 @@ Cart cart = Cart(
   note: '',
   totalCost: 0,
 );
-
-@unfreezed
-class CartCreation with _$CartCreation {
-  factory CartCreation({
-    @JsonKey(name: 'customer_id') String? customerId,
-    @JsonKey(name: 'list_order_product')
-    required List<OrderProduct> listOrderProduct,
-    String? note,
-  }) = _CartCreation;
-
-  factory CartCreation.fromJson(Map<String, Object?> json) =>
-      _$CartCreationFromJson(json);
-}
