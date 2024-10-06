eed39f15458f78c84d12f9862a71fe353486a81b
thaiha1607 <halequocthai@outlook.com>
2024-10-06 19:51:22 +0700

    Add dailyIncomeByRange

---
+++ lib/riverpod/daily_income.dart
@@ -0,0 +1,20 @@
+import 'package:foursquare/services/pb.dart';
+import 'package:foursquare/shared/models/daily_income.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'daily_income.g.dart';
+
+@riverpod
+Future<List<DailyIncomeDto>> dailyIncomeByRange(
+  DailyIncomeByRangeRef ref,
+  DateTime fromDate,
+  DateTime toDate,
+) async {
+  final fromDateTimeUtc = fromDate.toUtc().toString();
+  final toDateTimeUtc = toDate.toUtc().toString();
+  final records = await PBApp.instance.collection('daily_income').getFullList(
+        filter: 'created >= $fromDateTimeUtc && created <= $toDateTimeUtc',
+        sort: '-created',
+      );
+  return records.map((e) => DailyIncomeDto.fromRecord(e)).toList();
+}
