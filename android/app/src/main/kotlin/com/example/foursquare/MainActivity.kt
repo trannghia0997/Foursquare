4faa3571f009b4db19acf72ec58dc1c6594aa36b
thaiha1607 <halequocthai@outlook.com>
2024-10-19 07:35:47 +0700

    Update Gradle version

---
+++ android/app/src/main/kotlin/com/example/foursquare/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.foursquare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/foursquare_client/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.foursquare
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
